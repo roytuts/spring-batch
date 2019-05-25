0ba8be437c2fe564e8cdbad1a0da29c1a6df654d
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-05-25 14:15:12 +0530

    Add files via upload

---
+++ spring-batch-task-scheduler/src/main/java/com/jeejava/spring/batch/task/scheduler/SpringBatchTaskScheduler.java
@@ -0,0 +1,35 @@
+package com.jeejava.spring.batch.task.scheduler;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class SpringBatchTaskScheduler {
+
+	@Autowired
+	private Job job;
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Scheduled(cron = "*/10 * * * * *")
+	public void run() {
+		try {
+			JobExecution execution = jobLauncher.run(job,
+					new JobParametersBuilder().addLong("timestamp", System.currentTimeMillis()).toJobParameters());
+			System.out.println("Job Status : " + execution.getStatus());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		System.out.println("Done");
+	}
+
+}
