937539462a2b72329fd4f06629b46bd0cb72897c
Soumitra <projwebinfo@gmail.com>
2022-05-20 16:33:27 +0530

    Create SpringBatchCsvToXmlApp.java

---
+++ spring-batch-csv-to-xml/src/main/java/com/roytuts/spring/batch/csvtoxml/SpringBatchCsvToXmlApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.batch.csvtoxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCsvToXmlApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchCsvToXmlApp.class, args);
+	}
+
+}
