fdb9365d63c33d64e00208dd173642ae83dd0899
Soumitra <projwebinfo@gmail.com>
2022-05-23 14:59:20 +0530

    Create Person.java

---
+++ spring-batch-csv-to-mysql/src/main/java/com/roytuts/spring/batch/csvtomysql/vo/Person.java
@@ -0,0 +1,41 @@
+package com.roytuts.spring.batch.csvtomysql.vo;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+public class Person {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	@XmlAttribute(name = "id")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
