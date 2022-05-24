0782fd3dd3972bc39fdc848ce53deb2632ded210
Soumitra <projwebinfo@gmail.com>
2022-05-24 18:16:15 +0530

    Create PersonFieldSetMapper.java

---
+++ spring-batch-task-scheduler/src/main/java/com/roytuts/spring/batch/task/scheduler/fieldset/mapper/PersonFieldSetMapper.java
@@ -0,0 +1,20 @@
+package com.roytuts.spring.batch.task.scheduler.fieldset.mapper;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+
+import com.roytuts.spring.batch.task.scheduler.vo.Person;
+
+public class PersonFieldSetMapper implements FieldSetMapper<Person> {
+
+	@Override
+	public Person mapFieldSet(FieldSet fieldSet) {
+		Person person = new Person();
+		
+		person.setId(fieldSet.readInt(0));
+		person.setFirstName(fieldSet.readString(1));
+		person.setLastName(fieldSet.readString(2));
+		
+		return person;
+	}
+}
