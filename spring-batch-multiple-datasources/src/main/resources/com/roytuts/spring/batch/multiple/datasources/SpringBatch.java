0a674256e17840c93314daa3fa995e99a6fcb487
Soumitra <projwebinfo@gmail.com>
2023-08-21 19:35:05 +0530

    Create SpringBatch.java

---
+++ spring-batch-multiple-datasources/src/main/resources/com/roytuts/spring/batch/multiple/datasources/SpringBatch.java
@@ -0,0 +1,13 @@
+ package com.roytuts.spring.batch.multiple.datasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch.class, args);
+	}
+
+}
